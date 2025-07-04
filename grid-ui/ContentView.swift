0f7fef90f66875cce578220c6959994d96cfc3ea
Kaung-Htet-Hein <kaunghtethein.dev@gmail.com>
2025-07-04 20:57:50 +0800

    Initial Commit

---
+++ grid-ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  grid-ui
+//
+//  Created by Kaung Htet Hein on 04/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ grid-ui/grid_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  grid_uiApp.swift
+//  grid-ui
+//
+//  Created by Kaung Htet Hein on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct grid_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
